c0fdaf00803663166665d0e7e8a8425bcb7a58b8
OleksiiMishchenko <alex.mishch@gmail.com>
2025-02-12 19:17:03 +0200

    Added errors handling

--- app/components/makes-list.tsx
+++ app/components/makes-list.tsx
@@ -1,19 +1,37 @@
 import { Make, ApiResponse } from '@/types';
 
+import ErrorMessage from '@/app/components/error';
 import FilterForm from '@/app/components/filter-form';
 
-const fetchMakes = async (): Promise<Make[]> => {
-  const res = await fetch(
-    'https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json'
-  );
-  const data: ApiResponse<Make[]> = await res.json();
-  return data.Results;
+const fetchMakes = async (): Promise<Make[] | Error> => {
+  try {
+    const res = await fetch(
+      'https://vpic.nhtsa.dot.govs/api/vehicles/GetMakesForVehicleType/car?format=json'
+    );
+
+    if (!res.ok) {
+      throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+    }
+
+    const data: ApiResponse<Make[]> = await res.json();
+    return data.Results.length ? data.Results : new Error('No car makes found.');
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error fetching car makes:', error);
+      return error;
+    }
+    return new Error('An unknown error occurred.');
+  }
 };
 
 const MakesList = async () => {
-  const makes = await fetchMakes();
+  const result = await fetchMakes();
 
-  return <FilterForm makes={makes} />;
+  if (result instanceof Error) {
+    return <ErrorMessage message={result.message} />;
+  }
+
+  return <FilterForm makes={result} />;
 };
 
 export default MakesList;
